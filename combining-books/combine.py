747ac480bf2ce9346bc302c8c0193fad07de9e83
Mathuv <mathuvarman@yahoo.com>
2017-03-01 21:58:49 +1100

    Merging with pandas merge

--- combining-books/combine.py
+++ combining-books/combine.py
@@ -6,6 +6,20 @@ all_data = pd.DataFrame()
 for f in glob.glob("in\sales*.xlsx"):
     df = pd.read_excel(f)
     all_data = all_data.append(df, ignore_index=True)
-print(all_data.describe())
+
+# debug
+# print(all_data.describe())
+# print(all_data.head())
+
+#Conver the date column to a datetime object
+all_data['date'] = pd.to_datetime(all_data['date'])
+
+status = pd.read_excel("in/customer-status.xlsx")
+# status
+all_data_st = pd.merge(all_data, status, how='left')
+
+#all_data_st.head()
+
+
 
 pass
